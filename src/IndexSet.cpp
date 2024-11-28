8fea01e755869c103097e0e0081e541c63e0290e
nanocsv <138321428+nanocsv@users.noreply.github.com>
2024-11-28 20:09:36 +0800

    Delete src directory

--- src/IndexSet.cpp
+++
@@ -1,9 +0,0 @@
-#include "./lib/IndexSet.h"
-
-
-
-bitmapCompressSet* index::bitmapCompress(BitmapSet* BTMP, unsigned long length){
-    vector<vector<string>> index;
-    bitmapCompressSet* comBitmap;
-    return comBitmap;
-}
